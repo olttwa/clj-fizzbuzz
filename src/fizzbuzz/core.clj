f12b2e3d08c059b5bd45f9158c349d4f50a149b5
Akshat <akshat.shah@go-jek.com>
2017-03-27 00:29:42 +0530

    [Akshat] Implement fizzbuzz game

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,16 +1,29 @@
 (ns fizzbuzz.core
-  (:gen-class))
-
-(defn -main
-  [& args]
-  )
+  (:gen-class)
+  (require [clojure.string :as str]))
 
 (defn is-fizzy [number]
   (cond
     (= 0 (mod number 3)) "fizz"
-    :else ""))
+    :else nil))
 
 (defn is-buzzy [number]
   (cond
     (= 0 (mod number 5)) "buzz"
-    :else ""))
+    :else nil))
+
+(defn play-fizzbuzz
+  ([]
+   (play-fizzbuzz 100))
+  ([number]
+   (if (> number 1)
+     (play-fizzbuzz (- number 1)))
+   (let [response (str (is-fizzy number) (is-buzzy number))]
+     (-> response
+         (str/blank?)
+         (if number response)
+         (println)))))
+
+(defn -main
+  [& args]
+  (play-fizzbuzz ))
