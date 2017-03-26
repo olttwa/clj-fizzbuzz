256173407b682f85e9f14b24c0b180dce7b087f4
Akshat <akshat.shah@go-jek.com>
2017-03-26 23:35:04 +0530

    [Akshat] Add buzz decider function

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -9,3 +9,8 @@
   (cond
     (= 0 (mod number 3)) "fizz"
     :else ""))
+
+(defn is-buzzy [number]
+  (cond
+    (= 0 (mod number 5)) "buzz"
+    :else ""))
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -2,15 +2,30 @@
   (:require [clojure.test :refer :all]
             [fizzbuzz.core :refer :all]))
 
+(comment use generative testing to replace duplication)
+
 (deftest divisiblity-by-three
   (testing "fizz decider function"
     (let [multiple-of-three    12
-          non-mutiple-of-three 13]
+          non-multiple-of-three 13]
       (is (= (cond
                (= 0 (mod multiple-of-three 3)) "fizz"
                :else "")
              (is-fizzy multiple-of-three)))
       (is (= (cond
-               (= 0 (mod  non-mutiple-of-three 3)) "fizz"
+               (= 0 (mod  non-multiple-of-three 3)) "fizz"
                :else "")
-             (is-fizzy  non-mutiple-of-three))))))
+             (is-fizzy non-multiple-of-three))))))
+
+(deftest divisiblity-by-five
+  (testing "buzz decider function"
+    (let [multiple-of-five    10
+          non-multiple-of-five 13]
+      (is (= (cond
+               (= 0 (mod multiple-of-five 5)) "buzz"
+               :else "")
+             (is-buzzy multiple-of-five)))
+      (is (= (cond
+               (= 0 (mod  non-multiple-of-five 5)) "buzz"
+               :else "")
+             (is-buzzy non-multiple-of-five))))))
